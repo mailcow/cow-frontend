9f8525997c9baa4666d0e8c4dafab9e5c7016340
Ahmet <ahmetkucuk4@gmail.com>
2020-07-29 01:22:51 +0300

    Installed Axios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
-
 import router from './router';
 import store from './store';
-
 import App from './App.vue';
-
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+ 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 Vue.use(Buefy);
+
+axios.defaults.withCredentials = true;
 
 new Vue({
   router,
