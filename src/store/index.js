7dd4e8d6767ccd5dd1a8ffd84145e472fdcfe764
Ahmet <ahmetkucuk4@gmail.com>
2020-07-29 01:23:12 +0300

    Added Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import email from './modules/email';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  },
+	email
+  }
 });
---
+++ src/store/modules/email.js
@@ -0,0 +1,18 @@
+const state = {
+};
+
+const actions = {
+};
+
+const mutations = {
+};
+
+const getters = {
+};
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+};
