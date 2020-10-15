431b7cc6a542acfbf5cc6e8b377093bebe77ec59
Ahmet <ahmetkucuk4@gmail.com>
2020-10-15 23:20:08 +0300

    New config file

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,7 @@
 var path = require('path');
-const IS_LOCAL = process.env.NODE_ENV !== 'production';
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  runtimeCompiler: false,
-  devServer: {
-    disableHostCheck: true
-  },
-  productionSourceMap: false,
   configureWebpack: {
-    plugins: IS_LOCAL ? [] : [
-      new CompressionPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.(js|css|html|svg|png|jpg)$/,
-        threshold: 10240,
-        minRatio: 0.8,
-      }),
-      new CompressionPlugin({
-        filename: '[path].br[query]',
-        algorithm: 'brotliCompress',
-        test: /\.(js|css|html|svg|png|jpg)$/,
-        compressionOptions: {
-          level: 11,
-        },
-        threshold: 10240,
-        minRatio: 0.8,
-      })
-    ],
     resolve: {
       alias: {
         // Mailcow Aliases
