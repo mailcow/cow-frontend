40d6b98fd9d98afe7723d6773c435356e2e650a4
Ahmet <ahmetkucuk4@gmail.com>
2020-10-16 11:22:19 +0300

    New eslint conf remove editorconfing file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential'
-    // '@vue/airbnb',
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
