f280de403ce96578d3a396583cbb76ea87453c80
Ahmet <ahmetkucuk4@gmail.com>
2020-10-27 21:33:08 +0300

    Calendar navigation menu

---
+++ src/components/Calendar/NavigationMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside class="navigation-drawer" :class="{'open': $store.getters.navigation_drawer}">
+    <div class="block" style="padding: 1rem; margin-bottom: 0;">
+      <b-button type="is-primary" expanded icon-left="pencil">NEW EVENT</b-button>
+    </div>
+    <b-datepicker v-model="date"
+        class="sidemenu-calendar"
+        size="is-small"
+        locale="tr"
+        inline 
+        :first-day-of-week="1"
+        >
+    </b-datepicker>
+  </aside>
+</template>
+<script>
+export default {
+  data: () => ({
+    date: new Date()
+  })
+};
+</script>
