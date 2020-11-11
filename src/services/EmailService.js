0f1828a170fb3d8efa5f576320a854b09abce0dc
Ahmet <ahmetkucuk4@gmail.com>
2020-11-11 03:06:20 +0300

    Added new email services

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -19,5 +19,11 @@ export default {
   },
   delete_folder (folder_id) {
     return axios.delete(API_BASE_URL + 'email/folders/' + folder_id);
+  },
+  update_message (mail_id, data) {
+    return axios.put(API_BASE_URL + 'email/messages/' + mail_id + '/', data);
+  },
+  send_message (data) {
+    return axios.post(API_BASE_URL + 'email/send/', data);
   }
 };
