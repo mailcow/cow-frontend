9c2c6a53d53fbb67dfa5c6a6d4f409e0dac94dfc
Ahmet <ahmetkucuk4@gmail.com>
2020-11-11 03:08:04 +0300

    Added new store for account

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import auth from './modules/auth';
+import account from './modules/account';
 import theme from './modules/theme';
 import email from './modules/email';
 
@@ -10,6 +11,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     auth,
+    account,
     theme,
     email
   }
---
+++ src/store/modules/account.js
@@ -0,0 +1,55 @@
+const state = {
+  account_dialog: false,
+  active_account: null,
+  accounts: null
+};
+
+const actions = {
+
+  // Account Service post
+
+  change_account({commit}, account_id) {
+    commit('change_account', account_id);
+  }
+};
+
+const mutations = {
+  add_accounts (state, accounts) {
+    state.accounts = accounts;
+    localStorage.setItem('accounts', JSON.stringify(accounts));
+  },
+  set_active_account (state, active_account) {
+    if (!active_account) {
+      active_account = state.accounts.filter((ac) => {return ac.is_main})[0];
+      localStorage.setItem('active', JSON.stringify(active_account));
+    }
+    state.active_account = active_account;
+  },
+  change_account (state, account_id) {
+    var account = state.accounts.filter((a) => { return a.id === account_id})[0];
+    state.active_account = account;
+    localStorage.setItem('active', JSON.stringify(account));
+  },
+  account_dialog_status (state, status) {
+    state.account_dialog = status;
+  }
+};
+
+const getters = {
+  accounts (state) {
+    return state.accounts;
+  },
+  active_account (state) {
+    return state.active_account;
+  },
+  account_dialog (state) {
+    return state.account_dialog;
+  }
+};
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+};
