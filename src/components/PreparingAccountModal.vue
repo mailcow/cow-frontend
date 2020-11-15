e33dc703af40c9996dfe9c7d3917e74878e6ca60
Ahmet <ahmetkucuk4@gmail.com>
2020-11-16 02:15:43 +0300

    Added preparing account dialog component

---
+++ src/components/PreparingAccountModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-modal 
+      v-model="dialog"
+      has-modal-card
+      full-screen 
+      :can-cancel="false"
+    >
+      <div class="modal-card" style="width: auto">
+        <section class="modal-card-body">
+          <b-loading :is-full-page="true" v-model="dialog" :can-cancel="false"></b-loading>
+          <h1>Preparing your account</h1>
+        </section>
+      </div>
+  </b-modal>
+</template>
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Email.vue
+++ src/views/Email.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <navigation-menu></navigation-menu>
+    <preparing-account-modal :dialog="is_preparing_account"></preparing-account-modal>
+    <navigation-menu v-show="!is_preparing_account"></navigation-menu>
     <div class="columns is-gapless is-fullheight">
       <div class="column is-4 messages is-fullheight" id="message-feed">
         <email-list></email-list>
@@ -17,16 +18,26 @@
 import NavigationMenu from 'mailcow-components/Email/NavigationMenu';
 import EmailList from 'mailcow-components/Email/EmailList';
 import EmailContent from 'mailcow-components/Email/EmailContent';
+import PreparingAccountModal from 'mailcow-components/PreparingAccountModal';
 
 export default {
   name: 'Email',
   data: () => ({
+    is_preparing_account: false
   }),
   created () {
-    this.$store.commit('set_filter', {'in': this.$route.params.folder, 'limit': 20, 'offset': 0});
     this.$store.dispatch('get_folders');
-    this.$store.dispatch('get_total_message_count');
-    this.$store.dispatch('get_messages');
+    this.fetch_email();
+
+    // First login
+    if (this.$store.getters.get_folders.length === 0) {
+      this.is_preparing_account = true;
+      setTimeout(() => {
+        this.$store.dispatch('get_folders');
+        this.fetch_email();
+        this.is_preparing_account = false;
+      }, 5000);
+    }
   },
   props: {
   },
@@ -45,7 +56,8 @@ export default {
   components: {
     'navigation-menu': NavigationMenu,
     'email-list': EmailList,
-    'email-content': EmailContent
+    'email-content': EmailContent,
+    'preparing-account-modal': PreparingAccountModal
   },
   watch: {
     active_account () {
