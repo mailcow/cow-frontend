46526d386594825b292925709afec659a7a15130
Ahmet <ahmetkucuk4@gmail.com>
2021-01-16 19:46:09 +0300

    Changed Empty State components structure Added default state for pages

---
+++ src/components/EmptyState/State.vue
@@ -0,0 +1,36 @@
+<template>
+  <state-envelope v-if="mode === 'envelope'" :text="text"></state-envelope>
+  <state-cactus v-else-if="mode === 'cactus'" :text="text"></state-cactus>
+  <state-page v-else-if="mode === 'page'" :text="text" :image="image"></state-page>
+</template>
+<script>
+
+import Cactus from 'mailcow-components/EmptyState/States/Cactus';
+import Envelope from 'mailcow-components/EmptyState/States/Envelope';
+import Page from 'mailcow-components/EmptyState/States/Page';
+
+export default {
+  data: () => ({
+  }),
+  props: {
+    mode: {
+      type: String,
+      default: 'envelope'
+    },
+    image: {
+      type: String,
+      default: 'page.svg',
+      required: false
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    'state-envelope': Envelope,
+    'state-cactus': Cactus,
+    'state-page': Page,
+  }
+};
+</script>
