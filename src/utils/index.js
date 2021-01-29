c213839c62fd6aca821999bf1d5e0e21ecde9d3b
Ahmet <ahmetkucuk4@gmail.com>
2021-01-30 01:49:57 +0300

    Added timezones and langs utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,9 @@
 import filters_settings_schemas from './schemas/filters_settings.json';
+import timezones from './timezones';
+import langs from './langs';
 
 export {
-  filters_settings_schemas
+  filters_settings_schemas,
+  timezones,
+  langs
 };
---
+++ src/utils/langs.js
@@ -0,0 +1,7 @@
+const langs = [
+    {title: 'Türkçe', code: 90},
+    {title: 'Deutch', code: 49},
+    {title: 'English', code: 44},
+];
+
+export default langs;
